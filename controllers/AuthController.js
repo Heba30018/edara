4b8fa1a59f87732b4cbae5fc53a1bb2b24615431
Heba <hiba30018@gmail.com>
2023-04-03 23:38:29 +0200

    create Auth Controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,44 @@
+const session = require('express-session');
+const db =require('../config/db')
+const passwordHash = require('password-hash');
+
+class AuthController {
+
+    static async login(req, res,next){
+        var email = req.body.email;
+        var password = req.body.password;
+
+        if(email && password){
+            db.query("select * from supervisors where email = ? and password =?",[email,password],function(err,data){
+                if(data.length > 0){
+                    req.session.supervisor_id = data[0].id;
+                    res.send({ message: "login successful", session: req.session });
+                    console.log(session.supervisor_id)
+                }else{
+                    res.send({ message: "Incorrect email or password" });
+                }
+            })
+
+        }else{
+            res.send({message:"please enter your email and password"})
+            res.end();
+        }
+        
+    }
+
+    static async logout(req,res,next){
+        if (req.session) {
+            req.session.destroy(function(err) {
+                if (err) {
+                    next(err);
+                } else {
+                    res.send("logout succesfully");
+                }
+            });
+        } else {
+            res.send("already logged out");
+        }
+    }
+}
+
+module.exports=AuthController;
