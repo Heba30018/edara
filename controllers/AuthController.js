d0a0764a26dc2f7725cd9f3f79f5be17c934d8de
Heba <hiba30018@gmail.com>
2023-04-08 10:06:48 +0200

    feat: Add warehouse ID to session for supervisors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,21 +1,36 @@
 const session = require('express-session');
 const db =require('../config/db')
 
-class AuthController {
+const getWarehouseId=(supervisor_id)=>{
+    return new Promise (resolve =>{
+        db.query("select warehouse_id from warehouses where supervisor_id =?",[supervisor_id],(error,result)=>{
+            if(!error){
+                resolve(result[0].warehouse_id )
+            }
+        })
 
+        })
+
+ }
+class AuthController {
+    
     static async login(req, res,next){
         var email = req.body.email;
         var password = req.body.password;
 
         if(email && password){
-            db.query("select * from users where email = ? and password =?",[email,password],function(err,data){
+            db.query("select * from users where email = ? and password =?",[email,password],async function(err,data){
                 if(data.length > 0){
                     req.session.user_id = data[0].id;
                     if(data[0].type == 'admin'){
                         res.send({ message: "login successful Admin", session: req.session });
                     }
                     else{
+                        req.session.warehouse_id= await getWarehouseId(data[0].id)
+                      
                         res.send({ message: "login successful Supervisor", session: req.session });
+
+                        
                     }
                     
                 }else{
@@ -29,7 +44,7 @@ class AuthController {
         }
         
     }
-
+   
     static async logout(req,res,next){
         if (req.session) {
             req.session.destroy(function(err) {
