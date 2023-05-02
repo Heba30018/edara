f48db73e7dc4d39c6648ac39b51af21ebe038459
Amr Yousef <3mr.g4mer.channel@gmail.com>
2023-05-02 18:35:56 +0300

    fix: change photo data type to string

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
-const db =require('../config/db')
+const db = require('../config/db')
 
-class ProductModel  {
-    constructor(name='',description='',photo='',stock=''){
+class ProductModel {
+    constructor(name = '', description = '', photo = '', stock = '') {
         this.name = name;
         this.description = description;
         this.photo = photo;
@@ -9,73 +9,72 @@ class ProductModel  {
     }
 
 
-    getProducts(){
-        return new Promise (resolve =>{
-            db.query("select * FROM `products`",[],(error,result)=>{
-                if(!error){
-                    resolve(result )
+    getProducts() {
+        return new Promise(resolve => {
+            db.query("select * FROM `products`", [], (error, result) => {
+                if (!error) {
+                    resolve(result)
                 }
             })
-    
-            })
+
+        })
     }
 
     addNewProduct() {
-        return new Promise(resolve =>{
-            
-            db.query("INSERT INTO products (name, description, photo,stock) VALUES (?, ?, ?, ?)", [this.name, this.description,this.photo,this.stock],(error,result)=>{
-            if(!error){
-                resolve(true)
-            }
-            else{
-                console.log(error)
-                resolve(false)
-        
-            }
-        })
+        return new Promise(resolve => {
+
+            db.query("INSERT INTO products (name, description, photo,stock) VALUES (?, ?, ?, ?)", [this.name, this.description, this.photo, this.stock], (error, result) => {
+                if (!error) {
+                    resolve(true)
+                } else {
+                    console.log(error)
+                    resolve(false)
+
+                }
+            })
         });
-    
+
     }
-   
 
 
-    updateProduct(product_id,data){
-        return new Promise(resolve =>{
-           console.log(data.photo)
-           console.log(this.photo)
+    updateProduct(product_id, data) {
+        return new Promise(resolve => {
             db.query("UPDATE products SET? WHERE product_id = ?"
-            ,[{name:data.name,description:data.description,photo:this.photo,stock:data.stock},product_id],(error,result)=>{
-            if(!error){
-                resolve(true)
-            }
-            else{
-                resolve(false)
-            }
-    })
+                , [{
+                    name: data.name,
+                    description: data.description,
+                    photo: data.photo,
+                    stock: data.stock
+                }, product_id], (error, result) => {
+                    if (!error) {
+                        resolve(true)
+                    } else {
+                        resolve(false)
+                    }
+                })
 
-    }) 
-    
-    
+        })
+
+
     }
 
-    deleteProduct(delete_product_id){
-    
-        return new Promise(resolve =>{
-            db.query("delete from products where ?",{product_id:delete_product_id},(error,result)=>{
-            if(!error){
-                result.message = 'success'
-                resolve(true)
-            }
-            else{
-                resolve(false)
-            }
-    })
+    deleteProduct(delete_product_id) {
 
-    }) 
-    
+        return new Promise(resolve => {
+            db.query("delete from products where ?", {product_id: delete_product_id}, (error, result) => {
+                if (!error) {
+                    result.message = 'success'
+                    resolve(true)
+                } else {
+                    resolve(false)
+                }
+            })
+
+        })
+
     }
 
 }
 
 
-module.exports=ProductModel;
+module.exports = ProductModel;
